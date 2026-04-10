04f526eb845cd39cfdf7fb4a8a3c590c63000eec
sarah-bvrith517 <23wh1a0517@bvrithyderabad.edu.in>
2026-04-10 13:24:03 +0530

    Added Home Changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,6 +12,7 @@ function Home() {
       <p>Lab ID: {"FSDLAB2"}</p>
 
       {/* TODO: Replace this placeholder with your question set UI */}
+      <button onClick={handleEdit}>Edit Task</button>
       <p>QuestionComponent placeholder — implement your assigned question set here.</p>
 
     </div>
